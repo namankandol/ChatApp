604a30b10b46c77e771b09a92dfae25b056b8bc5
namankandol <82331362+namankandol@users.noreply.github.com>
2024-01-07 18:58:27 +0530

    host changed

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:4000"
+export const host = "https://chatapp-backend-koqa.onrender.com"
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
